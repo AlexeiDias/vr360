80098e04e709b63d0cd74fa92a87f831945f4b66
AlexeiDias <alexeidias@gmail.com>
2022-04-19 08:15:12 -0300

    title change

--- src/components/Topcontainer/TopContainer.js
+++ src/components/Topcontainer/TopContainer.js
@@ -11,7 +11,7 @@ const TopContainer = () => {
     <div class="container-fluid text-center colorTop" bgc >
   <div class="row">
     <div class="col">
-    <h3 class="my-2">Recurso Visual 360 - Conquiste novos clientes com Tour Virtual 360</h3>
+    <h3 class="my-2">Shopping Realidade Virtual 360</h3>
     </div>
     </div>
 </div>
